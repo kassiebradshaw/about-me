adec44b16e3c0df811d4d99ec58c0e3007fc2984
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2021-01-13 21:38:19 -0800

    finished assignment

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# about-me
+# LAB - 02
+
+## About Me 
+
+<!-- I created a short "about me" quiz that asks the user 5 questions about me and lets them know if they are correct. Then my page gives a bit of my background. -->
+
+### Author: Kassie Bradshaw 
+
+### Links and Resources
+* [submission PR](http://xyz.com)
+* [CSS reset] (http://meyerweb.com/eric/tools/css/reset/ )
+
+### Reflections and Comments and Collaborations
+* Consider including the answers to your daily journal and submission questions here
+* This is also a good place to reflect on the tools and resources used and learned
---
+++ js/app.js
@@ -0,0 +1,78 @@
+'use strict';
+
+// get username and offer greeting
+var userName = prompt('Hello, and thank you for taking my quiz! What is your name?');
+alert('Hello, ' + userName + ', nice to meet you!');
+
+alert('Please answer the following "yes or no" questions...');
+
+// write 5 questions. they MUST accept yes or no OR y or n IN ANY CASE -case-sensitivity needs to work in every format
+// examples: YES yes Yes NO no No, Y, y
+
+// this is question 1
+var answerLive = prompt('Do I live in Washington?').toLowerCase();
+
+if (answerLive === 'no' || answerLive === 'n') {
+  // console.log('Do I live in Washington? ' + answerLive);
+  // if answer is no, this will show up
+  alert('Correct! I live in Oregon.');
+} else {
+  // console.log('Do I live in Washington? ' + answerLive);
+  // otherwise, this will show up
+  alert('I live in Oregon.');
+}
+
+// this is question 2
+var answerCode102 = prompt('Did I take Code 102?').toLowerCase();
+
+if (answerCode102 === 'no' || answerCode102 === 'n') {
+  // console.log('Did I take Code 102? ' + answerCode102);
+  // if answer is no, this will show up
+  alert('Correct! I came to Code 201 from Code 101.');
+} else {
+  // console.log('Did I take Code 102 ' + answerCode102);
+  // otherwise, this will show up
+  alert('I came to Code 201 from Code 101.');
+}
+
+// this is question 3
+var answerColor = prompt('Is my favorite color green?').toLowerCase();
+
+if (answerColor === 'no' || answerColor === 'n') {
+  // console.log('Is my favorite color green? ' + answerColor);
+  // if answer is no, this will show up
+  alert('Correct! My favorite color is yellow.');
+} else {
+  // console.log('Is my favorite color green? ' + answerColor);
+  // otherwise, this will show up
+  alert('My favorite color is yellow.');
+}
+
+// this is question 4
+var answerPets = prompt('Do I have any pets?').toLowerCase();
+
+if (answerPets === 'yes' || answerPets === 'y') {
+  // console.log('Do I have any pets? ' + answerPets);
+  // if answer is yes, this will show up
+  alert('Yep! I have 2 cats.');
+} else {
+  // console.log('Do I have any pets? ' + answerPets);
+  // otherwise, this will show up
+  alert('I have 2 cats.');
+}
+
+// this is question 5
+var answerCoffee = prompt('Do I like coffee?').toLowerCase();
+
+if (answerCoffee === 'yes' || answerCoffee === 'y') {
+  // console.log('Do I like coffee? ' + answerCoffee);
+  // if answer is yes, this will show up
+  alert('Absolutely. Coffee is life.');
+} else {
+  // console.log('Do I like coffee? ' + answerCoffee);
+  // otherwise, this will show up
+  alert('Coffe is life.');
+}
+
+//final message to user
+alert('Thanks again ' + userName + ' for taking my quiz!');
